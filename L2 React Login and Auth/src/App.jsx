25a8e26fe0db9273507b82a89496fec7c570f821
Joy Brar <joybrar2001@gmail.com>
2024-04-22 16:05:18 +0530

    Lesson 1

---
+++ L2 React Login and Auth/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Register from './Register'
+import Login from './Login'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  )
+}
+
+export default App
